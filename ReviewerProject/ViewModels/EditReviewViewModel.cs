b14e5e2a069ff7d48e088d19c1c931839a5ee714
Denfit9 <79207007+Denfit9@users.noreply.github.com>
2024-11-09 22:21:48 +0300

    Update EditReviewViewModel.cs

--- ReviewerProject/ViewModels/EditReviewViewModel.cs
+++ ReviewerProject/ViewModels/EditReviewViewModel.cs
@@ -5,7 +5,9 @@ namespace ReviewerProject.ViewModels
     public class EditReviewViewModel
     {
         public Guid Id { get; set; }
+        [MaxLength(80, ErrorMessage = "Maximum 80 character long")]
         public string? Name { get; set; }
+        [MaxLength(1099, ErrorMessage = "Maximum 1099 character long")]
         public string? Description { get; set; }
         public int? Rating { get; set; }
         public List<int> Ratings { get; set; }
